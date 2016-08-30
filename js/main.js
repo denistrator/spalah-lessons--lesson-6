704f39868a57355da6a9493d8bf87e577d650805
Denis <denistrator@yandex.ua>
2016-08-30 13:31:25 +0300

    add jquery and my scrypt

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {		
+	$('.card__img-holder').height($('.card').height());
+	$('.card__img-inner').height($('.card').height());
+	$('.card__content').height($('.card').height());
+	$('.card__content-inner').height($('.card').height());
+});
