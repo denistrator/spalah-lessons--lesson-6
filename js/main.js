b1e278f6c361d9dff4c34d03a56fe31d6b9cbe18
Denis <denistrator@yandex.ua>
2016-08-31 14:17:42 +0300

    minor fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {		
-	$('.card__img-holder').height($('.card').height());
-	$('.card__img-inner').height($('.card').height());
-	$('.card__content').height($('.card').height());
-	$('.card__content-inner').height($('.card').height());
+	$('.card__img-holder').height($('.card').height() + 1);
+	$('.card__img-inner').height($('.card').height() + 1);
+	$('.card__content').height($('.card').height() + 1);
+	$('.card__content-inner').height($('.card').height() + 1);
 });
