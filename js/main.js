afc8235c78cbed2a3bf4c9a120f50401e0a15747
Denis <denistrator@yandex.ua>
2016-09-01 17:29:47 +0300

    add smooth scrolling for nav links. now links realy work

--- js/main.js
+++ js/main.js
@@ -3,4 +3,13 @@ $( document ).ready(function() {
 	$('.card__img-inner').height($('.card').height() + 1);
 	$('.card__content').height($('.card').height() + 1);
 	$('.card__content-inner').height($('.card').height() + 1);
+
+	$("a").click(function () {
+		$(".navigation__elem").removeClass('navigation__elem_active');
+		$(this).parent().addClass('navigation__elem_active');
+		var elementClick = $(this).attr("href");
+		var destination = $(elementClick).offset().top;
+		$('html,body').animate( { scrollTop: destination }, 1000 );
+		return false;
+	});
 });
